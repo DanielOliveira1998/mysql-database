30f53a7cec4b8e908bb545125a7bfe2b6e449797
Daniel Oliveira <danoliveira865@gmail.com>
2023-04-17 18:37:12 -0300

    Req 7 finalizado

--- challenges/desafio7.sql
+++ challenges/desafio7.sql
@@ -1,14 +1,3 @@
--- SELECT
---   Artistas.Nome AS artista,
---   Albuns.Nome AS album,
---   COUNT(Artistas_Seguidos.Pessoas_Usuarias_Id) AS pessoas_seguidoras
--- FROM 
---   Artistas INNER JOIN 
---   Artistas_Seguidos ON Artistas.Id = Artistas_Seguidos.Artistas_Id
---   INNER JOIN 
---   Pessoas_Usuarias ON Artistas_Seguidos.Pessoas_Usuarias_Id = Pessoas_Usuarias.Id
---   GROUP BY Artistas.Nome
---   ORDER BY pessoas_seguidoras DESC, artista;
 SELECT
   Artistas.Nome AS artista,
   Albuns.Nome AS album,
@@ -16,7 +5,7 @@ SELECT
 FROM
   Albuns INNER JOIN
   Artistas ON Albuns.Artistas_Id = Artistas.Id
-  LEFT JOIN 
+  LEFT JOIN
   Artistas_Seguidos ON Artistas.Id = Artistas_Seguidos.Artistas_Id
-  GROUP BY Artistas.Nome
-  ORDER BY pessoas_seguidoras DESC, artista;
+  GROUP BY Artistas.Nome, Albuns.Nome
+  ORDER BY pessoas_seguidoras DESC, Artistas.Nome, Albuns.Nome;
